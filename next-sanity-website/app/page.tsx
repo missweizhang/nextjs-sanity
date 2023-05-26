bd5b1d6c8fa1e8d2a5ec0d05efbafd26f2699327
Wei Zhang <missweizhang@gmail.com>
2023-05-25 19:53:49 -0700

    add project info and images to homepage

--- next-sanity-website/app/page.tsx
+++ next-sanity-website/app/page.tsx
@@ -1,7 +1,46 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
+  return (
+    <div className="max-w-5xl mx-auto">
+      <h1 className="text-7xl font-extrabold">
+        Hello{" "}
+        <span className="bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-600 bg-clip-text text-transparent">
+          HCA
+        </span>
+      </h1>
 
-  return projects.map((project) => <div key={project._id}>{project.name}</div>);
+      <p className="mt-3 text-xl text-gray-600">
+        Aloha everyone! Check out our new events!
+      </p>
+
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">New Events</h2>
+
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {projects.map((project) => (
+          <Link
+            href={`/projects/${project.slug}`}
+            key={project._id}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={750}
+                height={300}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div className="mt-2 font-extrabold bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-600 bg-clip-text text-transparent">
+              {project.name}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
